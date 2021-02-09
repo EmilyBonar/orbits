8e2f5597c9291541bf549b19395d032c6aa476fb
emilybonar <emilykbonar@gmail.com>
2021-02-08 19:24:10 -0600

    Made orbiter work at any size

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./App.css";
 
 function App() {
-	const size = "200px";
+	const size = 400;
 	return (
 		<div className="flex flex-row w-screen h-screen bg-indigo-900">
 			<div className="grid flex-1 place-items-center">
 				<div className="" style={{ width: size, height: size }}>
 					<Path />
-					<Orbiter />
+					<Orbiter size={size} />
 				</div>
 			</div>
 			<div className="h-full bg-white w-96"></div>
@@ -24,18 +24,18 @@ function Path() {
 	);
 }
 
-function Orbiter() {
+function Orbiter(props: { size: number }) {
 	return (
 		<div
 			className="relative w-full h-full col-span-1 row-span-1"
 			style={{
-				top: "-200px",
-				animation: "orbit 2s linear infinite",
+				top: `-${props.size}px`,
+				animation: "reverse-orbit 2s linear infinite",
 			}}
 		>
 			<div
 				className="relative w-8 h-8 origin-center bg-black rounded-full top-1"
-				style={{ left: "83px" }}
+				style={{ left: `${props.size / 2 - 32 / 2}px` }}
 			></div>
 		</div>
 	);
